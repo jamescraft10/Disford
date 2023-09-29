9f55ad88bb6c5a2c1463e55f6b095748b3ec4347
JamesCraft10 <69867626+James251133@users.noreply.github.com>
2023-09-28 21:57:44 -0400

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -4,17 +4,27 @@ const http = require('http');
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
+const fs = require("fs");
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/Docs/index.html');
+  fs.readFile(__dirname + '/Docs/index.html', (err, data) => {
+    if (err) throw err;
+    res.write(data);
+
+    fs.readFile(__dirname + '/db.txt', (err, data) => {
+      if (err) throw err;
+      res.end(data);
+    });
+  });
 });
 
 app.get('/Scripts/main.js', (req, res) => {
-    res.sendFile(__dirname + "/Scripts/main.js");
+  res.sendFile(__dirname + "/Scripts/main.js");
 });
 
 io.on('connection', (socket) => {
     socket.on('chat message', (msg) => {
+        fs.appendFileSync(__dirname + '/db.txt', `<script>AddText(\'${msg}\');</script>`); // Puts Message In File
         io.emit('chat message', msg);
     });
 });
